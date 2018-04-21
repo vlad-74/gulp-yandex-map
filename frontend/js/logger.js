68264736b7fdb4a30cea385676e5f742ef67b19a
Vlad <kurguzov74@gmail.com>
2018-04-21 16:11:13 +0300

    first commit

---
+++ frontend/js/logger.js
@@ -0,0 +1,18 @@
+// var log = require('logger')(module);
+// var args = [' - logger = ' + module.filename.substring(module.filename.indexOf('##log1Module\\dir') +17)].concat([].slice.call(arguments));
+global.accessLoger = false;
+
+module.exports = function(module) {
+  return function(/* ... */) {
+
+  console.log.format = function (c) {
+    return c.getDate() + ": [" + c.filename + ":" + c.getLineNumber() + "] " + c.functionName;
+  };
+
+  	const isDev = process.env.NODE_ENV;
+    var args = [' logger: ' + JSON.stringify(arguments) + ' || ' ];
+    	if(accessLoger){
+    		console.log.apply(console, args);
+    	} 
+  };
+};
